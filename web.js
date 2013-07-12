e5ce8d1eb5bf1fa73b8f96abbd99414810ec54f2
Konkura <phil@konkura.com>
2013-07-12 09:08:28 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -8,8 +8,7 @@ app.get('/', function(request, response) {
         
     fs.readFileSync('index.html', function (err, data) {
 	if (err) throw err;
-	var buf=new Buffer(data,"utf-8");
-	response.send(buf.toString("utf-8"));
+	response.send(buf.toString('utf8'));
     });
 
 });
