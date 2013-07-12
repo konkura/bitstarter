a4651d3df365e0b9ad88cf89e507e691dad8ddc2
Konkura <phil@konkura.com>
2013-07-12 08:58:45 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,15 +5,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    
-response.send('Here');
-    
+        
     fs.readFileSync('index.html', function (err, data) {
 	if (err) throw err;
 	response.send(data.toString("utf-8"));
     });
 
-//    response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
