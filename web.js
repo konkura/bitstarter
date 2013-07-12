1b72a5dda1be504de201c0a968a0dc749bc3bfff
Konkura <phil@konkura.com>
2013-07-12 09:17:30 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,10 +6,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
         
-    fs.readFileSync('index.html',{encoding: 'utf8'}, function (err, data) {
-	if (err) throw err;
-	response.send(data.toString());
-    });
+    var data=fs.readFileSync('index.html',{encoding: 'utf8'});
+	response.send(data);
+  
 
 });
 
